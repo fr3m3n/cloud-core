9e40b550a239d732f165193318e37fd3e53ead32
fr3m3n <mtimochenko@tutanota.com>
2023-10-28 23:54:27 -0300

    [update] +EC2 +S3 instances

--- main.tf
+++ main.tf
@@ -5,3 +5,39 @@
 # Version: 1.0
 # ==================================================================
 
+
+# EC2: Public
+resource "aws_instance" "ec2_public" {
+    ami             = "${var.image_id}"
+    instance_type   = "t2.micro"
+    subnet_id       = aws_subnet.public_subnet.id
+    count           = var.ec2_public_count
+
+    tags = {
+        Name = "${var.ec2_public_name}-${count.index + 1}"
+    }
+}
+
+# EC2: Private
+resource "aws_instance" "ec2_private" {
+    ami           = "${var.image_id}"
+    instance_type = "t2.micro"
+    subnet_id       = aws_subnet.private_subnet.id
+    count         = var.ec2_private_count
+
+    tags = {
+        Name = "${var.ec2_private_name}-${count.index + 1}"
+    }
+}
+
+
+# S3: Storage
+resource "aws_s3_bucket" "s3" {
+    bucket = "${var.s3_name}-${count.index + 1}"
+    count  = var.s3_count
+
+    tags = {
+        Name        = "CloudCore_bucket_${count.index + 1}"
+        Environment = "Dev"
+    }
+}
